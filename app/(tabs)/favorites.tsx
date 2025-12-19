58c24b0050d190faf7fb6eb92ce1a517c0356883
Doğukan ÖKSÜZ <oksuz.dogukan@gmail.com>
2025-12-19 12:39:00 +0300

    first commit

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,12 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const index = () => {
+export default function favorites() {
   return (
     <View>
-      <Text>index</Text>
+      <Text>favorites</Text>
     </View>
   );
-};
-
-export default index;
+}
