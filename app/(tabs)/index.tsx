9ec9efc100e0d69fd0f86c478d49e1b30f0f11ab
Doğukan ÖKSÜZ <oksuz.dogukan@gmail.com>
2025-12-22 14:47:23 +0300

    feat: added states for camera permission

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/index.styles";
+import { useAction } from "convex/react";
+import { useCameraPermissions } from "expo-camera";
+import React, { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function index() {
+export default function Index() {
+  // STATE MANAGMENT
+  const [permission, requestPermission] = useCameraPermissions();
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [recipes, setRecipes] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const cameraRef = useRef(null);
+  const analyzeImage = useAction(api.gemini.analyzeImage); // gemini function from convex
+
+  // Camera permissions
+  if (!permission) return <View />;
+  if (!permission.granted) {
+    return (
+      <View style={styles.cameraPermissionContainer}>
+        <Text>Kamera izni gerekiyor</Text>
+        <TouchableOpacity onPress={requestPermission}>
+          <Text>İzin Ver</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
     <View>
       <Text>index</Text>
