8d828b4bf5687a23563b48141e23618f1cd90f35
Doğukan ÖKSÜZ <oksuz.dogukan@gmail.com>
2025-12-24 12:19:55 +0300

    feat: added hooks for taking picture and analyz picture

---
+++ hooks/useCameraSystem.ts
@@ -0,0 +1,41 @@
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import { Alert } from "react-native";
+
+export default function useCameraSystem() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const cameraRef = useRef<CameraView>(null);
+
+  // Camera permission
+  const handleCameraPermission = async () => {
+    if (!permission) return; // if user did not give permission
+
+    // user has already given permission
+    if (permission.granted) {
+      setIsCameraOpen(true);
+      return;
+    }
+
+    // if user did not give permisson, request permission
+    const result = await requestPermission();
+    if (result.granted) {
+      setIsCameraOpen(true);
+    } else {
+      Alert.alert(
+        "İzin Gerekli",
+        "Buzdolabını tarayabilmemiz için kamera izni gerekli."
+      );
+    }
+  };
+
+  const closeCamera = () => setIsCameraOpen(false);
+
+  return {
+    permission,
+    isCameraOpen,
+    cameraRef,
+    handleCameraPermission,
+    closeCamera,
+  };
+}
